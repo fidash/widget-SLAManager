c33cd2cb8643898553d65583a4640769fdbd1c75
Borja Illescas Medina <billescas@conwet.com>
2015-09-07 13:43:29 +0200

    Code refactoring. Retrieving templates data

--- src/js/SLAManager.js
+++ src/js/SLAManager.js
@@ -26,6 +26,7 @@
     var SLAManager = function SLAManager() {
         this.statusSelect = null;
         this.agreementsData = null;
+        this.templatesData = null;
         this.agreementsTable = document.getElementById("agreements_table");
     };
 
@@ -54,33 +55,63 @@
         this.status_select.addEntries(statuses);*/
     //};
 
-    var getAgreements = function getAgreements() {
-        var url = MashupPlatform.prefs.get("server_url");
-        if (url[url.length - 1] !== "/") {
-            url += "/";
+    var makeRequest = function makeRequest(url, method, onSuccess, onFailure) {
+        var baseURL = MashupPlatform.prefs.get("server_url");
+        if (baseURL[baseURL.length - 1] !== "/") {
+            baseURL += "/";
         }
-        url+="agreements";
+        baseURL += url;
 
-        MashupPlatform.http.makeRequest(url, {
-            method: "GET",
+        MashupPlatform.http.makeRequest(baseURL, {
+            method: method,
             requestHeaders: {
                 user: MashupPlatform.prefs.get("user"),
                 password: MashupPlatform.prefs.get("password"),
                 Accept: "application/json"
             },
-            onSuccess: function (response) {
-                setAgreementsData.call(this, JSON.parse(response.response));
-            }.bind(this),
-            onFailure: function () {
-                console.log("Failing on the request");
-            }
+            onSuccess: onSuccess,
+            onFailure: onFailure
         });
+    }
+
+    var getAgreements = function getAgreements() {
+
+        makeRequest.call(this, "agreements", "GET",
+            onSuccess: function (response) {
+                setAgreements.call(this, JSON.parse(response.response));
+                getTemplates.call();
+            }.bind(this),
+
+            onFailure: function () {
+                console.log("Error retrieving the agreements data");
+            }
+        );
     };
 
-    var setAgreementsData = function setAgreementsData(agreementsData) {
-        this.agreementsData = agreementsData;
-        displayData.call(this, agreementsData);
-    };
+    var getTemplates = function getTemplates() {
+
+        makeRequest.call(this, "templates", "GET",
+            onSuccess: function (response) {
+                this.templatesData = JSON.parse(response.response);
+                displayData.call(this, agreementsData);
+            }.bind(this),
+            onFailure: function () {
+                console.log("Error retrieving the templates data");
+            }
+        );
+    }
+
+    var setAgreements = function setAgreements(response){
+        this.agreementsData = {};
+
+        for (var i in response){
+            this.agreementsData[response[i]['id']] = {
+                context: response[i]['context'],
+                name: response[i]['name'],
+                terms: response[i]['terms']
+            };
+        }
+    }
 
     var displayData = function displayData(data) {
         //TODO How to update data???
@@ -91,14 +122,20 @@
         console.log(tableData);
 
     };
-
+    /*Status    Actions Agreement Name  Template Name   Provider    Service    */
     var transformData = function transformData(data) {
+
         var transformedData = [];
         for (var i in data) {
             var row = [];
-            for (var d in data[i]) {
-                row.push(data[i][d]);
-            }
+            //Apparently we need to get the status separately, does not make much sense....
+            row.push("Fullfiled"); //?? STATUS
+            row.push("Actions"); //Not sure what goes in here
+            row.push(data[i]['name']);//Agreement name
+            row.push(data[i]['template']);//Template Name
+            row.push(data[i]['name']);//Provider
+            row.push(data[i]['name']);//Service
+
             transformedData.push(row);
         }
 
