b3621fa54b8eb87738532f31b1fe4c622d45e08f
bgrana <bgrana@conwet.com>
2015-10-01 12:36:38 +0200

    Add karma dependencies and configuration file

---
+++ karma.conf.js
@@ -0,0 +1,67 @@
+ module.exports = function(config) {
+    "use strict";
+
+    config.set({
+      frameworks: ['jasmine', 'fixture'],
+    
+      singleRun: true,
+
+      browsers: ['Firefox', 'Chrome'],
+
+      files: [
+
+        // Source code
+        'src/js/SLAManager.js',
+
+        // Spec files
+        'src/test/js/*Spec.js'
+      ],
+
+      preprocessors: {
+        'src/js/**/*.js': ['coverage']
+      },
+
+      exclude: [
+        'src/js/main.js'
+      ],
+
+      plugins: [
+        'karma-jasmine',
+        'karma-firefox-launcher',
+        'karma-chrome-launcher',
+        'karma-phantomjs-launcher',
+        'karma-fixture',
+        'karma-junit-reporter',
+        'karma-coverage'
+      ],
+
+      reporters: ['progress', 'junit', 'coverage'],
+
+      junitReporter: {
+        outputDir: 'build/test-reports'
+      },
+
+      coverageReporter: {
+        reporters: [ 
+          {
+            type : 'html',
+            dir : 'build/coverage/',
+            subdir: 'html'
+          },
+          {
+            type: 'cobertura',
+            dir: 'build/coverage',
+            subdir: 'xml'
+          },
+          {
+            type: 'json',
+            dir: 'build/coverage',
+            subdir: 'json'
+          },
+          {
+            type: 'text-summary'
+          }
+        ]
+      }
+    });
+  };
